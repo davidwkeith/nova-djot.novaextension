7dcb29e77c1f2bda55458e96bf6d016e6c88f910
David W. Keith <git@dwk.io>
2026-04-13 14:35:27 -0700

    feat: add Djot code folding query

---
+++ Queries/folds.scm
@@ -0,0 +1,21 @@
+; Sections fold as heading + all content until next same-or-higher heading
+(section) @fold
+
+; Fenced code and raw blocks
+(code_block) @fold
+(raw_block) @fold
+
+; Lists (bullet, ordered, task, definition)
+(list) @fold
+
+; Fenced div containers
+(div) @fold
+
+; Block quotations
+(block_quote) @fold
+
+; Tables
+(table) @fold
+
+; Footnote definitions
+(footnote) @fold
