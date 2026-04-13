0a0a697713fcdb4f900c31cec09ed0435da55258
David W. Keith <git@dwk.io>
2026-04-13 14:35:29 -0700

    feat: add Djot symbol navigation for headings

---
+++ Queries/symbols.scm
@@ -0,0 +1,8 @@
+; Headings appear in Nova's Symbols sidebar for document navigation
+(heading
+    (marker) @context.begin
+    (_) @name
+) @symbol
+
+; Sections create nesting hierarchy (H2 under H1, H3 under H2, etc.)
+(section) @subtree
